17b521f3223ab726b55175fb5ea79ebd686969f6
YeHwanLee <139995082+YeHwanLee@users.noreply.github.com>
2026-04-17 15:20:44 +0900

    Update Web.jsx

--- src/pages/Projects/Web.jsx
+++ src/pages/Projects/Web.jsx
@@ -23,7 +23,7 @@ function ComputerModel() {
           {/* 실제 웹사이트를 띄우는 iframe */}
           <iframe
             title="Embedded Web"
-            src="https://ohleeohlee.github.io/portfolio" // 내 포트폴리오 주소!
+            src="https://https://ohleeohlee-test.github.io/Test_Web/" // 내 포트폴리오 주소!
             style={{
               width: '1024px',  // 실제 웹 가상 해상도
               height: '670px',
